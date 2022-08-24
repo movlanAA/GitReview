a580e2ca21cbb291c141eac76b7cf3c38315989b
Movlan Aliyev <movlan@live.com>
2022-08-23 23:06:44 -0700

    3.adding Bye world

---
+++ src/first/ByeWorld.java
@@ -0,0 +1,8 @@
+package first;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("5 weeks java left");
+        System.out.println("1.5 weeks of GitHub");
+    }
+}
